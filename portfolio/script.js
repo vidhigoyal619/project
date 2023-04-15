2172e18b80c4a352ae6c94136a204960301c3d8a
vidhigoyal619 <126322705+vidhigoyal619@users.noreply.github.com>
2023-04-15 01:56:42 -0800

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//     $('#menu').click(function(){
+//         $(this).toggleclass('fa-times');
+//         $('header').toggleclass('toggle');
+//     });
+//     $(window).on('scroll load', function(){
+//         $('#menu').toggleclass('fa-times');
+//         $('header').toggleclass('toggle');
+//     });
+// });
